4da19708c55633e9539d751c53d7ab937d65352b
Rajeshwar Guddeti <149294175+Rajeshwar1018@users.noreply.github.com>
2025-03-20 06:48:57 +0000

    Committed

---
+++ Basics/Datatypes.js
@@ -0,0 +1,18 @@
+let name="Rajesh"//string datatype
+let age=18//integer
+let isLongedIn=false//boolean
+
+
+/*
+Different datatypes in javaScript
+
+number,
+bigint,
+String=>" "
+boolean=> true/false
+null=> standalone value
+undefined=> empty 
+Symbol=> unique
+object
+
+*/
