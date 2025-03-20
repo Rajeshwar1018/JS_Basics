d6ea5660b93123d0d1982d5b3b7c1e3ae032c7a8
Rajeshwar Guddeti <149294175+Rajeshwar1018@users.noreply.github.com>
2025-03-20 05:50:00 +0000

    commited

---
+++ Basics/Variables.js
@@ -0,0 +1,13 @@
+const accountId=145555555
+let accountEmail="sanjana@gmail.com"
+var accountPassword="12345"
+accountCity="Malad-Mumbai"
+let accountstate;
+
+
+accountEmail="rishi@gmail.com"
+accountPassword="123456789"
+accountCity="NaderGul-Hyderabad"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountstate])
+
