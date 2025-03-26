0cca02c776298c4460d48f3630fc8c456adaaa6f
Rajeshwar Guddeti <149294175+Rajeshwar1018@users.noreply.github.com>
2025-03-26 09:16:27 +0000

    Commited

---
+++ Basics1/Array.js
@@ -0,0 +1,57 @@
+const arr=[0,1,2,3,4,5]
+// console.log(arr)
+
+// console.log(typeof arr)
+
+const arr1=new Array(0,1,2,3,4,5,)
+//console.log("A "+ arr1)
+
+arr.push(6)
+arr.pop()
+
+//console.log("B "+ arr)
+
+arr.unshift(9)
+//console.log("C "+ arr)
+
+arr.shift()
+//console.log("D "+ arr)
+
+
+//console.log(arr2)
+
+
+
+//console.log(arr3)
+
+
+const arr2= arr.slice(0,3)
+// console.log(arr2)
+// console.log(arr)
+
+
+const arr3= arr.splice(0,3)
+console.log(arr3)
+console.log(arr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
