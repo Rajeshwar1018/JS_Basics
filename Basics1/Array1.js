2cfed0bc8dc424f24f70078d92dfb652f25b92b2
Rajeshwar Guddeti <149294175+Rajeshwar1018@users.noreply.github.com>
2025-03-26 11:07:16 +0000

    done

---
+++ Basics1/Array1.js
@@ -0,0 +1,20 @@
+const M_Heros=['rajesh', 'rajeshwar', 'ruh']
+const F_Heroines=['Sanju', 'Sanjana', 'rui']
+
+console.log(M_Heros)
+console.log(F_Heroines)
+M_Heros.push(F_Heroines)
+
+// console.log(M_Heros)
+// console.log(M_Heros[3][2])
+
+const Con=M_Heros.concat(F_Heroines)
+
+
+
+
+
+
+
+
+
