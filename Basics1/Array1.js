59036be3915a43be26b0c7ae129694fe763fabe3
Rajeshwar Guddeti <149294175+Rajeshwar1018@users.noreply.github.com>
2025-03-27 06:16:38 +0000

    Commited

--- Basics1/Array1.js
+++ Basics1/Array1.js
@@ -1,14 +1,33 @@
 const M_Heros=['rajesh', 'rajeshwar', 'ruh']
 const F_Heroines=['Sanju', 'Sanjana', 'rui']
 
-console.log(M_Heros)
-console.log(F_Heroines)
-M_Heros.push(F_Heroines)
+// console.log(M_Heros)
+// console.log(F_Heroines)
+// M_Heros.push(F_Heroines)
 
 // console.log(M_Heros)
 // console.log(M_Heros[3][2])
 
-const Con=M_Heros.concat(F_Heroines)
+const All_actors=M_Heros.concat(F_Heroines)
+// console.log(All_actors)
+
+const AnotherArray=[1,2,3,[4,5,6],7,[8,9],10]
+
+const AnotherAgain=AnotherArray.flat(Infinity)
+// console.log(AnotherAgain)
+
+console.log(Array.isArray("Sanjana"))
+console.log(Array.from("Sanjana"))
+
+
+let s1=100
+let s2=200
+let s3=300
+
+console.log(Array.of(s1,s2,s3))
+
+
+
 
 
 
