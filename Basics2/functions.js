1b27772ae9a8feb31a38b3d9ccd3d0abb67eb7bd
Rajeshwar Guddeti <149294175+Rajeshwar1018@users.noreply.github.com>
2025-03-28 07:33:24 +0000

    committed

--- Basics2/functions.js
+++ Basics2/functions.js
@@ -12,18 +12,43 @@
 function Addnumbers(num1, num2){
     console.log(num1+num2)
 }
-// Addnumbers(3,4)
+// Addnumbers(3,4)7
 
 function Addnumber(num1, num2){
     let result=num1+num2
     return result
 }
 const result=Addnumber(12,34)
-// console.log(result)
+// console.log(result)46
+
+function Addnumber2(num1, num2){
+    return num1+num2   
+}
+const result2=Addnumber(26,34)
+// console.log(result2)
 
 
 function IsloggedIn(username){
-    let name=username
     return `${username} is loggedIn`
 }
-cons
+//console.log(IsloggedIn())//undefined is loggedIn
+
+function IsloggedIn(username){
+    return `${username} is loggedIn`
+}
+//console.log(IsloggedIn(""))// is loggedIn
+
+function IsloggedIn(username){
+    return `${username} is loggedIn`
+}
+//console.log(IsloggedIn("rajesh"))//rajesh is loggedIn
+
+
+function IsloggedIn(username){
+    if(username==undefined)
+    {
+        console.log("Please enter the username")
+    }
+    return `${username} is loggedIn`
+}
+//console.log(IsloggedIn())//Please enter the username undefined is loggedIn
