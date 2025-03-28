4222f50dcda251504c019de4066d0600d704c582
Rajeshwar Guddeti <149294175+Rajeshwar1018@users.noreply.github.com>
2025-03-28 07:19:33 +0000

    commited

---
+++ Basics2/functions.js
@@ -0,0 +1,29 @@
+// function MyName(){
+//     console.log("R")
+//     console.log("A")
+//     console.log("J")
+//     console.log("E")
+//     console.log("S")
+//     console.log("H")
+// }
+
+// MyName()
+
+function Addnumbers(num1, num2){
+    console.log(num1+num2)
+}
+// Addnumbers(3,4)
+
+function Addnumber(num1, num2){
+    let result=num1+num2
+    return result
+}
+const result=Addnumber(12,34)
+// console.log(result)
+
+
+function IsloggedIn(username){
+    let name=username
+    return `${username} is loggedIn`
+}
+cons
