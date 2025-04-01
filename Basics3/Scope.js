4dbdd31d2a015f21c6a2bfa34d22a4e4dbbc8422
Rajeshwar Guddeti <149294175+Rajeshwar1018@users.noreply.github.com>
2025-04-01 10:11:43 +0000

    done

---
+++ Basics3/Scope.js
@@ -0,0 +1,60 @@
+if(true){
+    let a=3
+    const b=4
+    var c=6
+   
+}
+
+//console.log(a) error
+//console.log(b) error
+//console.log(c) //6
+
+
+//+++++++++++++++++++++++++++++++
+function one(){
+    const username="rajesh"
+    function two(){
+        const website="youtube"
+        console.log(username)
+    }
+    //console.log(website)
+    //two() rajesh
+}
+one()
+
+
+//+++++++++++++++++++++++++++++++++++++
+function addtwo(num)
+{
+    return num+1
+}
+//console.log(addtwo(5))//6
+
+
+//console.log(addtwo(6))//7
+function addtwo(num)
+{
+    return num+1
+}
+
+const addthree=function(num1){
+    return num1+2
+}
+
+console.log(addthree(6))//8
+
+
+
+console.log(addthree1(6))//cannot access
+const addthree1=function(num1){
+    return num1+2
+}
+
+
+
+
+
+
+
+
+
